5756d11e1c3c4a38a7739c176d04e5433b5c52d8
daniellachu <delacruzdanielleb@gmail.com>
2023-03-13 10:11:36 -0700

    bcit page translation

---
+++ .next/static/webpack/pages/_app.7ee6c12ca80b095b.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./i18n/en.json":
+/*!**********************!*\
+  !*** ./i18n/en.json ***!
+  \**********************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"page.home.head.navbar.learnhub":"Learning Hub","page.home.head.navbar.mybcit":"MyBCIT","page.home.head.navbar.students":"Students","page.home.head.navbar.staff":"Staff","page.home.head.navbar.events":"Events","page.home.menu.program":"Programs & Courses","page.home.menu.admission":"Admission","page.home.menu.studentservice":"Student Services","page.home.menu.research":"Research","page.home.menu.about":"About BCIT","page.home.breadcrumbs.study":"Study","page.home.breadcrumbs.b&m":"Business & Media","page.home.head.title":"Digital Design And Development","page.home.hero.meta.diploma":"Diploma","page.home.hero.meta.fulltime":"Full-Time","page.home.hero.meta.sb&m":"School of Business + Media","page.home.banner.button.apply":"Apply Now","page.home.banner.button.contact":"Contact Us","page.home.sidenav.aboutprogram":"About this program","page.home.sidenav.overview":"Overview","page.home.sidenav.entrancerequirements":"Entrance Requirements","page.home.sidenav.advancedplacement":"Advanced Placement","page.home.sidenav.costandsupplies":"Cost & Supplies","page.home.sidenav.programdetails":"Program details","page.home.sidenav.graduating":"Graduating & Jobs","page.home.sidenav.contact":"Contact Us","page.home.sidenav.subscribe":"Subscribe","page.home.info.sessions":"Info Sessions","page.home.info.sessions.message":"Attend an upcoming information session for this program:","page.home.info.date.april":"April 3rd at 5:30pm","page.home.info.date.may":"May 8th at 5:30pm","page.home.info.date.june":"June 19th at 5:30pm","page.home.info.date.july":"July 10th at 5:30pm","page.home.info.explore":"Explore all upcoming BCIT info sessions.","page.home.info.programoverview":"Program Overview","page.home.info.credential":"Credential:","page.home.info.credential.diploma":"Credential Diploma","page.home.info.format":"Format:","page.home.info.format.full-time":"Full-time","page.home.info.length":"Length:","page.home.info.length.years":"2 years","page.home.info.startdate":"Start Date:","page.home.info.startdate.sept":"September","page.home.info.campus":"Campus:","page.home.info.campus.burnaby":"Burnaby","page.home.head.title.old":"Next.js i18n example","page.home.head.navbar":"Learning Hub","page.home.head.meta.description":"Next.js i18n example - English","page.home.title":"Welcome to the home page","page.home.head.description":"You are currently viewing the home page in English"}');
+
+/***/ })
+
+});
